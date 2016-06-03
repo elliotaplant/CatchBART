1bd346a23f727f35afba5dd612b0a8b32da3b8b6
Elliot Plant <elliotaplant@gmail.com>
2016-06-03 13:51:39 -0700

    Following tutorial

--- WeatherDemo/ViewController.swift
+++ WeatherDemo/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityTempLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getWeatherData("http://api.openweathermap.org/data/2.5/weather?q=London,uk")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getWeatherData(urlString: String) {
+        let url = NSURL(string: urlString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
+            dispatch_async(dispatch_get_main_queue(), {
+                self.setLabels(data!)
+            })
+        }
+        
+        task.resume()
+    }
 
+    func setLabels(weatherData: NSData) {
+        var jsonError: NSError?
+        
+        let json = NSJSONSerialization.JSONObjectWithData(weatherData, options: nil, error: &jsonError) as NSDictionary
+        
+        if let name = json["name"] as? String {
+            cityNameLabel.text = name
+        }
+        
+        if let main = json["main"] as? NSDictionary {
+            if let temp = main["temp"] as? Double {
+                cityTempLabel.text = String(format: "%.1f", temp)
+            }
+        }
+        
+    }
 }
 
