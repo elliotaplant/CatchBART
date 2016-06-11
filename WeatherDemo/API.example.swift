9858c42cafcc413d9fe86669143fab2a561b8696
Elliot Plant <elliotaplant@gmail.com>
2016-06-11 10:18:38 -0700

    Begin hiding API key

---
+++ WeatherDemo/API.example.swift
@@ -0,0 +1,13 @@
+//
+//  APIkey.example.swift
+//  WeatherDemo
+//
+//  Created by Elliot Plant on 6/11/16.
+//  Copyright © 2016 Elliot Plant. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    let baseUrl = 
+}
